c0991646689e19044fbf40706cec8c8f343c1075
pedromndz.pro <p.menendez.pro@gmail.com>
2025-10-08 18:01:36 +0200

    Deploy to GitHub Pages

---
+++ vite.config 2.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: "/Boulangerie_template/", // 👈 ajoute cette ligne ! (nom EXACT de ton repo GitHub)
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
